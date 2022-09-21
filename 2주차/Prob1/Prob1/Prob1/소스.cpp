f90abfd68774b2beab3576f7c076cf2ad6b4afc5
Sonyoungjin <ysjsjs1000@naver.com>
2022-09-21 10:21:07 +0900

    2 - prob1
    printf 연습

---
+++ 2주차/Prob1/Prob1/Prob1/소스.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("class number : %d\n", 2019011051);     // 2019011051 output to %d
+	printf("name : %s\n", "sonyoungjin");  // sonyoungjin output to %s
+	printf("grade : %c\n", 'A');            // A output to %c
+	return 0;
+}
