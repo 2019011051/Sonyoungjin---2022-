ed6a270153330d33458923160187cfd95967f7fe
Sonyoungjin <ysjsjs1000@naver.com>
2022-09-21 11:51:41 +0900

    수정
    단위변환

--- 2주차/Prob1/Prob4/Prob4/소스.cpp
+++ 2주차/Prob1/Prob4/Prob4/소스.cpp
@@ -7,8 +7,9 @@ int main(void)
 	double Bmi;               //Specifying Bmi Variables
 
 
-	printf("input height and m : ");                   // Input height and m
+	printf("input height and kg : ");                   // Input height and m
 	scanf_s("%d%lf", &Kg, &m);
+	double Heightm = (Kg * 0.01);
 	Bmi = (Kg) / (m * m);                              // Bmi Calculation
 	double res = (Bmi >= 20) && (Bmi < 25) ? printf("표준체중입니다") : printf("체중관리가필요합니다");
 
