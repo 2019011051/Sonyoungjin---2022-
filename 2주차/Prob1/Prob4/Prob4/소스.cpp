534d7779753065d0d8d8f4c39b204503760bd604
Sonyoungjin <ysjsjs1000@naver.com>
2022-09-21 10:50:20 +0900

    2-Prob4

---
+++ 2주차/Prob1/Prob4/Prob4/소스.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int Kg;                   //Specify a weight variable
+	double m;                 //Height Specify a variable
+	double Bmi;               //Specifying Bmi Variables
+
+
+	printf("input height and m : ");                   // Input height and m
+	scanf_s("%d%lf", &Kg, &m);
+	Bmi = (Kg) / (m * m);                              // Bmi Calculation
+	double res = (Bmi >= 20) && (Bmi < 25) ? printf("표준체중입니다") : printf("체중관리가필요합니다");
+
+	return 0;
+
+}
