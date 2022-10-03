652cac4844685f03e4a49ab8bcffe52bb4ca4dc4
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-03 21:55:19 +0900

    3-1장 확인문제 3번

---
+++ 혼자 공부/3장 p72 확인문제3번/3장 p72 확인문제3번/소스.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int kor = 70;
+	int eng = 80;
+	int mat = 90;
+	int total = 0;
+
+	total = (kor + eng + mat);
+	printf("kor : %d",70);
+	printf("eng : %d",80);
+	printf("mat : %d\n",90);
+	printf("total : %d\n", total);
+	return 0;
+
+}
