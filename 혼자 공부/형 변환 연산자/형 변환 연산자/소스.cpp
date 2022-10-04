1ae86e0fcbfebc7c58783740c9ac5dcefdf9cf84
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-04 23:14:55 +0900

    .1을 왜 써야하는지 몰랐으나 오류 고치면서 이해함.

---
+++ 혼자 공부/형 변환 연산자/형 변환 연산자/소스.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 20;
+	int b = 3;
+	double res;
+    
+	res = ((double)a / (double)b);
+	printf("a = %d, b= %d\n", a, b);
+	printf("a/b : %.1lf\n", res);
+
+	a = (int)res;
+	printf("(int) %.1lf : %d\n", res, a);
+
+	return 0;
+
+
+	
+}
