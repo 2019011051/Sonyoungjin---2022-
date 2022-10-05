154f941ec4aef91cd17c59f5dbfe34e22bf7ebca
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-05 17:35:10 +0900

    책을 통해 코딩을하며 for문을 이해 했지만 나중에 혼자 해볼 예정.

--- 5주차/구구단/구구단/구구단.cpp
+++ 5주차/구구단/구구단/구구단.cpp
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j;         //Specify variables i, j
 
-	for (i = 2; i<=9; i++)
+	for (i = 2; i<=9; i++)        
 	{
 		for (j = 1; j <= 9; j++)
 		{
-			printf("%d*%d= %d\n", i, j, i * j);
+			printf("%d*%d= %d\n", i, j, i * j);     //Utilize repeat control variables i and j
 		}
 	}
 
