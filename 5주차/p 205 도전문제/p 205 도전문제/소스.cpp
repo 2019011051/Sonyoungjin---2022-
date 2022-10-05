80dd5db9ecaf8f61950416a6da308ee9fad7e528
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-05 17:16:44 +0900

    재귀호출의 의미는 이해 했지만 변수지정할때 잘 이해가 되지 않아서 힘들었음.

--- 5주차/p 205 도전문제/p 205 도전문제/소스.cpp
+++ 5주차/p 205 도전문제/p 205 도전문제/소스.cpp
@@ -4,6 +4,16 @@ int rec_function(int n);
 
 int main(void)
 {
-	rec_function(1);
+	int total;                  // Specify Total Variables
+	int a;                      // Specify the variable for rec_function(n+1)
+	total = rec_function(1);    // Give 1 as an argument because it is the first time to call.
+	printf("%d\n", total);      //Total Output
 	return 0;
 }
+int rec_function(int n)         //Store call count in parameters
+{
+	if (n == 10) return 10;      // If the number of calls is 10, the recursive call ends
+	int a = rec_function(n+1);   // Store a value in variable a that increases the number of calls by 1 when re-calling
+	int total = a + n;           //When the number of re-calls reaches 10, the calculation is made and the recursive call is completed.
+}
+	
