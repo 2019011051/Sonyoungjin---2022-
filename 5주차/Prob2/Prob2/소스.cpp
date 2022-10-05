552fcc2d8293fb261e476df86ab9e054ffd24798
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-05 17:51:07 +0900

    cnt 부분에서 정말 많이 헷갈렸음. 친구의 도움으로 이해 했음.

--- 5주차/Prob2/Prob2/소스.cpp
+++ 5주차/Prob2/Prob2/소스.cpp
@@ -2,28 +2,28 @@
 
 int main(void)
 {
-	int i;
-	int n;
-	int k;
-	int cnt = 0;
+	int i;                    // Specify a variable
+	int n;                   //Specify a variable
+	int k;                     //Specify a variable
+	int cnt = 0;                 //Specify a variable
 
-	printf("Enter an integer greater than or equal to 2 : ");
-	scanf_s("%d", &k);
+	printf("Enter an integer greater than or equal to 2 : ");     // "input Enter an integer greater than or equal to 2 "
+	scanf_s("%d", &k);                                          // input your k
 	
-	for (i = 2; i <=k; i++)
+	for (i = 2; i <=k; i++)  
 	{
 		for (n = 2; n <=i; n++)
 		{
-			if (i%n==0)
+			if (i%n==0)                         // If i divided by n and the rest is zero,                          
 			{
-				if (i>n) break;
+				if (i>n) break;           // If i is greater than n, escape the inner repeat door
 
-				else
+				else                     // If i divided by n and the rest is zero,
 				{
-					printf("%5d", i);
-					cnt++;
-					if(cnt % 5 == 0)
-					printf("\n");
+					printf("%5d", i);      // 5 i-output with space
+					cnt++;                 // CNT rear-end increment/decrease operator
+					if(cnt % 5 == 0)       //   If you divide the cnt by 5 and divide it
+					printf("\n");          //  Line out
 				}
 
 			}
