cd15a6bd57fe6fc14054288a02de11267cfcb058
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-08 23:02:49 +0900

    if 중첩문

---
+++ 혼자 공부/중첩된 if문/중첩된 if문/소스.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 20;
+	int b = 10;
+
+	if (a >= 10)
+	{
+		if (b > 0)
+		{
+			b = 1;
+		}
+		else
+		{
+			b = 0;
+		}
+	}
+
+	printf("a : %d, b : %d\n", a, b);
+	return 0;
+
+}
