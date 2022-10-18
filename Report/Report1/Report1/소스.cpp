0a859dda87dbe6cf3abcc32038d9ab362f0c2d58
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-18 19:32:13 +0900

    (최종)파일로 옮겨서 진행

--- Report/Report1/Report1/소스.cpp
+++ Report/Report1/Report1/소스.cpp
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+
+int PIN_Program(void);
 
 int main(void)
 {
-	char str[80] = "7042";
-    
+	PIN_Program();
+	
+	return 0;
+}
+
+int PIN_Program(void)
+{
+	const char C_Password[10]= "7042";
+	char M_Password[10]= {0};
+
+	
+	while(1)
+	{
+		printf("Input your Password : ");
+		scanf_s("%s", M_Password, 10);
+		
+		if (strcmp(C_Password, M_Password) == 0)break;
+		
+		else
+		{
+			printf("not matched, retry...\n");
+		}
+		
+	}
+	printf("normal termination");
+ return 0;
 }
