7384e65895c081c523580ba46a073faac16de08e
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-25 16:01:55 +0900

    Report 1 주석 달기

--- Report/Report 1 (최종)/Report 1 (최종)/소스.cpp
+++ Report/Report 1 (최종)/Report 1 (최종)/소스.cpp
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-#define PIN "7042"     // PIN넘버 7042로 정의
-#define Array_size 10  // 배열크기 10으로 정의
+#define PIN "7042"     // Defined by PIN number 7042
+#define Array_size 10  // Defined by Array Size 10
 
-int PIN_Program(void);    // 함수 선언
+int PIN_Program(void);    // Declaration of functions
 
 int main(void)
 {
-    PIN_Program();             // 함수 호출 
+    PIN_Program();             // Call a function
     return 0;
 }
 
 int PIN_Program(void)
 {
-    char C_Password[10] = { PIN };   // 배열C_Password 배열을 10 하고 정의 내린 PIN 7 0 4 2 그뒤로는 널.
-    char M_Password[10] = "";  // M_Password 는 널로 초기화
+    char C_Password[Array_size] = { PIN };   // Array C_Password array 10 and defined PIN 70 42 followed by you.
+    char M_Password[Array_size] = "";  // M_Password initializes to null
 
 
-    while (1)    //무한반복문 시작
+    while (1)    //Start of infinite repetition
     {
         printf("Input your Password : ");    // Input your Password
-        scanf_s("%s", M_Password, 10);       // 
+        scanf_s("%s", M_Password, Array_size);       
         
-        if (strcmp(C_Password, M_Password) == 0)break;  //만약  C_Password 와 M_Password가 같다면 반복문 탈출
+        if (strcmp(C_Password, M_Password) == 0)break;  //If C_Password and M_Password are the same, escape the repeat door
 
-        else                                      // 같지 않다면 
+        else                                      // if not the same
         {
-            printf("not matched, retry...");     // not matched,retry.. 출력
+            printf("not matched, retry...");     // not matched,retry.. Output
         }
 
     }
-    printf("normal termination");           //normal termination 출력
+    printf("normal termination");           //normal termination Output
     return 0;
 }
