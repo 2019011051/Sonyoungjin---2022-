2c268dd929ca07335231e26047af5d1103a4d6da
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-25 16:13:43 +0900

    Report3 주석

--- Report/Report3/Report3/소스.cpp
+++ Report/Report3/Report3/소스.cpp
@@ -9,50 +9,50 @@
 
 void GenRandSeed();     // generating the seed number using the time clock information
 unsigned int GenRandNum(unsigned int nRange);  // generating the random number
-int Line_up_Program();        // 줄바꾸기 프로그램 선언
-void Multi_Matrix(int A_nRange[Line_number][Row_number], int B_nRange[Line_number][Row_number]);   //곱셈 행렬 프로그램 선언
+int Line_up_Program();        // Declaring a new line program
+void Multi_Matrix(int A_nRange[Line_number][Row_number], int B_nRange[Line_number][Row_number]);   //Multiplication Matrix Program Declaration
 
 int main (void)
 {
 	GenRandSeed();    // assigning seed
-	int Line = zero;           // 행 변수 선언
-	int Row = zero;              // 열 변수 선언
-	int A_nRange[Line_number][Row_number] = { zero };        // A행렬의 배열 선언
-	int B_nRange[Line_number][Row_number] = { zero };         // B행렬의 배열 선언
+	int Line = zero;           // Declare line variables 
+	int Row = zero;              //Declare Row variables
+	int A_nRange[Line_number][Row_number] = { zero };        // Declare the array of A matrices
+	int B_nRange[Line_number][Row_number] = { zero };         // Declare the array of B matrices
+
 
 	printf("A-Matrix");                    
 	printf("\n");
 	
-	for (Line = zero; Line < Max; Line++)      //  무작위 숫자 A행렬에 입력 
+	for (Line = zero; Line < Max; Line++)      //  Put in a random number A matrix 
 	{
-		printf("[");                       // 각 행앞에 [ 출력
+		printf("[");                       //Before each row [ output
 		for (Row = zero; Row < Max; Row++)
 		{
 			A_nRange[Line][Row] = rand() % 100;         
-			printf("%d\t", A_nRange[Line][Row]);           // A행렬 출력
+			printf("%d\t", A_nRange[Line][Row]);           // A matrix output
 		}
-		printf("]");                            // 각 행앞에 ] 출력
+		printf("]");                            // After each row ] output
 		printf("\n");
 	}
 	
-	Line_up_Program();              // 라인 띄우기 프로그램 함수 호출
-
+	Line_up_Program();              // Call line float program function
  	printf("B-Matrix");
 	printf("\n");
 
-	for (Line = zero; Line < Max; Line++)           // 무작위 숫자 B행렬에 입력 
+	for (Line = zero; Line < Max; Line++)           // Enter into a random number B matrix 
 	{
-		printf("[");                            // 각 행앞에 [ 출력
+		printf("[");                            // Before each row [ output
 		for (Row = zero; Row < Max; Row++)
 		{
 			B_nRange[Line][Row] = rand() % 100;
-			printf("%d\t", B_nRange[Line][Row]);           // B행렬 출력
+			printf("%d\t", B_nRange[Line][Row]);           //B matrix output
 		}
-		printf("]");                                   // 각 행앞에 ] 출력
+		printf("]");                                   // After each row ] output
 		printf("\n");
 	}
 	Line_up_Program();
-	Multi_Matrix(A_nRange, B_nRange);                 //곱셈 행렬 프로그램 호출
+	Multi_Matrix(A_nRange, B_nRange);                 //Invoke multiplication matrix program
 	return 0;
 }
 
@@ -71,7 +71,7 @@ unsigned int GenRandNum(unsigned int nRange) // generating the random number
 	return nRes;
 }
 
-// 줄 띄우기 함수
+// Line Plot Function
 int Line_up_Program()         
 {
 	int Line_up = 0;
@@ -84,7 +84,7 @@ int Line_up_Program()
 	return 0;
 }
 
-// 두 행렬의 곱 구하는 함수 매개변수 A_nRange[5][5], int B_nRange[5][5]
+// The product of two matrices function parameters A_nRange[5][5], int B_nRange[5][5]
 void Multi_Matrix(int A_nRange[Line_number][Row_number], int B_nRange[Line_number][Row_number])
 {
 	int Mult=0;
@@ -94,7 +94,7 @@ void Multi_Matrix(int A_nRange[Line_number][Row_number], int B_nRange[Line_numbe
 	int Row;
 	int X;
 	
-	for (Line = zero; Line < Max; Line++)                // 앞에 선언한 A행렬과 B행렬의 곱을 나타내는 식
+	for (Line = zero; Line < Max; Line++)                // An expression that represents the product of the previously declared matrix A and B
 	{
 		for (Row = zero; Row < Max; Row++)
 		{
@@ -109,7 +109,7 @@ void Multi_Matrix(int A_nRange[Line_number][Row_number], int B_nRange[Line_numbe
 	printf("A_Matrix X B_Matrix : ");
 	printf("\n");
 
-	for (Line = zero; Line < Max; Line++)     // 곱한 행렬 값 출력
+	for (Line = zero; Line < Max; Line++)     // Multiplied matrix value output
 	{
 		printf("[");
 		for (Row = zero; Row < Max; Row++)
