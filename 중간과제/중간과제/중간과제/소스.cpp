4de73181986b3879f50111628689f60ed132af9e
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-30 22:33:13 +0900

    .

--- 중간과제/중간과제/중간과제/소스.cpp
+++ 중간과제/중간과제/중간과제/소스.cpp
@@ -5,8 +5,11 @@ int main(void)
 	int Matrix[4][4];
 	int Row_Number, Line_Number;
     
-	for (Row_Number = 0; Row_Number < 4; Row_Number++)
+	for (Line_Number = 0; Line_Number < 4; Line_Number++)
 	{
-		for(Linbe)
+		for (Row_Number = 0; Row_Number < 4; Row_Number++)
+		{
+			printf
+		}
 	}
 }
