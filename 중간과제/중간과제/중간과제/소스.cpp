296bc60c1e2496474dbf219790fb0fe4417e7cd9
Sonyoungjin <ysjsjs1000@naver.com>
2022-10-30 23:39:09 +0900

    .

--- 중간과제/중간과제/중간과제/소스.cpp
+++ 중간과제/중간과제/중간과제/소스.cpp
@@ -2,14 +2,41 @@
 
 int main(void)
 {
-	int Matrix[4][4];
+	int Matrix[3][3]= {0};
+	int Solution[10]= {0};
+	int Solution_N;
 	int Row_Number, Line_Number;
-    
-	for (Line_Number = 0; Line_Number < 4; Line_Number++)
+
+	printf("Input your equation Number");
+
+	for (Line_Number = 0; Line_Number < 3; Line_Number++)
 	{
-		for (Row_Number = 0; Row_Number < 4; Row_Number++)
+		
+		for (Row_Number = 0; Row_Number < 3; Row_Number++)
 		{
-			printf
+			scanf_s("%d", &Matrix[Line_Number][Row_Number]);
 		}
 	}
+	printf("Input your solution of equations");
+
+	for (Solution_N = 0; Solution_N < 3; Solution_N++)
+	{
+		scanf_s("%d", &Solution);
+	}
+
+	for (Line_Number = 0; Line_Number < 3; Line_Number++)
+	{
+		printf("{");
+		for (Row_Number = 0; Row_Number < 3; Row_Number++)
+		{
+			printf("%5d\t", Matrix[Line_Number][Row_Number]);
+		}
+		printf("}\n");
+	}
+	for (Solution_N = 0; Solution_N < 3; Solution_N++)
+	{
+		printf("%5d", Solution[Solution_N]);
+	}
+
+
 }
