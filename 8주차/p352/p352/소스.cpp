40d20861f9576405356bfbe6fd37103a5e033697
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-09 11:27:01 +0900

    /

--- 8주차/p352/p352/소스.cpp
+++ 8주차/p352/p352/소스.cpp
@@ -3,11 +3,16 @@
 
 int main(void)
 {
-	char str[80];
-	strcpy_s(str, "wine");
+    char str[30];
+	char str1[5]= {0};
+	
+	strcpy_s(str, "ine");
+
 	strcat_s(str, "apple");
-	strncpy_s(str, "pear", 1);
-	printf("%s, %d\n", str, strlen(str));
+
+	strncpy_s(str1, "pear", 1);
+	
+	printf("%s%s, %d\n",str1, str, strlen(str1));
 	
 	return 0;
 }
