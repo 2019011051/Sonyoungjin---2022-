390a74df235578b7798ff9a9537c4190f8b06dbb
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-09 11:41:38 +0900

    설정 문제

--- 8주차/P353/P353/소스.cpp
+++ 8주차/P353/P353/소스.cpp
@@ -5,19 +5,18 @@ int main(void)
 {
 	char fruit1[10]= "0";
 	char fruit2[10] = "0";
-	char fruit3[10]= "0";
+	char fruit3[10]=  "0";
+	
 
-	scanf_s("%s\n", fruit1);
+	scanf_s("%s\n", fruit1[10]);
 	
 	printf("단어입력 : %s\n", fruit1);
 	if (strlen(fruit1) > 5)
 	{
 		strncat_s(fruit1, "*", 1);
 	}
-	else
-	{
-		printf("입력한 단어 : %s, 생략한 단어 : %s ", fruit1, fruit1);
-	}
+	printf("입력한 단어 : %s, 생략한 단어 : %s ", fruit1, fruit1);
+	
 	return 0;
 
 }
