524e99caf1a201b83f1b39511f5973884f47ce43
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-09 15:26:07 +0900

    프로젝트>>속성>>sdl검사를 아니오 라고 바꾸면 scanf scnpy등 _s를 사용하지 않고 원활한 출력이 가능한 것을 알게 되었음

--- 8주차/P353/P353/소스.cpp
+++ 8주차/P353/P353/소스.cpp
@@ -3,26 +3,28 @@
 
 int main(void)
 {
-	char fruit[80]={0};
+	//variation declare
+	char fruit[80]={0};        
 	char res_fruit[80];
-	char star[20] = "*******";
+	char star[80] = "*******";
 	int len=0;
 	
 	printf("단어입력 : ");
-	scanf("%s", fruit);
+	scanf("%s", fruit);               // Enter information to enter the fruit array
 	
-	len = strlen(fruit);
-	if (len<= 5)
+	len = strlen(fruit);             //Store the length value of the fruit array in len
+
+	if (len<= 5)                    // If len is less than or equal to 5,
 	{
-		strcpy(res_fruit, fruit);
+		strcpy(res_fruit, fruit);          // Save fruit in res_fruit
 	}
-	else
+	else  
 	{
-		strncpy(res_fruit, fruit, 5);
-		res_fruit[5]= '\0';
-		strncat(res_fruit, star, len - 5);
-	}
-	printf("입력한 단어 : %s, 생략한 단어 : %s\n", fruit, res_fruit);
+		strncpy(res_fruit, fruit, 5);               //Copy only 5 characters from fruit and save to res_fruit
+		res_fruit[5]= '\0';                             //res_fruit Save your text in the rest
+		strncat(res_fruit, star, len - 5);               //Fill the string with stars as long as the length of a string
+	} 
+	printf("입력한 단어 : %s, 생략한 단어 : %s\n", fruit, res_fruit);      
 	
 	return 0;
 
