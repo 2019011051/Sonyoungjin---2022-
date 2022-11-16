32a34edf524bc44b16b6312137b4ad701a884ad2
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-16 12:48:14 +0900

    .

---
+++ 9주차/494.2/494.2/소스.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+struct cracker
+{
+	int price;
+	int calories;
+};
+
+int main(void)
+{
+	struct cracker 바사삭;
+	printf("바사삭의 가격과 열랑을 입력하시오 : ");
+	scanf_s("%d%d", &(바사삭.price), &(바사삭.calories));
+
+	printf(" 바사삭의 가격 : %d", 바사삭.price);
+	printf(" 바사삭의 열량 : %dkcal", 바사삭.calories);
+
+	return 0;
+
+
+}
