fb17f072b9d7269f00fc7bbf272984f85e2f7155
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-22 20:09:50 +0900

    열거형에 대한 이해 <<꽤나 힘들었음.

--- 9주차/511-3/511-3/소스.cpp
+++ 9주차/511-3/511-3/소스.cpp
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum { CYAN, MAGENTA, YELLOW = 5, BLACK} COLOR;
-typedef enum { UP, DOWN, LEFT, RIGHT} ARROW;
+typedef enum { CYAN, MAGENTA, YELLOW = 5, BLACK} COLOR;   // Enumeration Declaration
+typedef enum { UP, DOWN, LEFT, RIGHT} ARROW;             //Enumeration Declaration
 
 int main(void)
 {
-	int c;
+
+	// variable declaration
+	int c;   
 	int direction = UP;
 
-	COLOR my_color = YELLOW;
+	COLOR my_color = YELLOW;       //Enumeration variable declaration
 	
 	
 
-	for (c = CYAN; c <= BLACK; c++)
+	for (c = CYAN; c <= BLACK; c++)    //Repeat 7 times from 0 to 6
 	{
-		direction++;
-		direction = direction % 4;
-		if (c == my_color)break;
+		direction++;                 //direction 1 increase
+		direction = direction % 4;     //The direction is rotated to 1,2,3,0,1,2,3
+		if (c == my_color)break;         //End repeat at 5 because my_color is yellow
 	}
 	
-	switch (direction)
+	switch (direction)   //Determining Enumeration Members
 	{
-	case UP: printf("현재 방향 : 위"); break;
-	case DOWN: printf("현재 방향 : 아래"); break;
-	case LEFT: printf("현재 방향 : 왼쪽"); break;
-	case RIGHT: printf("현재 방향 : 오른쪽"); break;
+	case UP: printf("Current direction: Above"); break;     //"Current direction: Above" output when up
+	case DOWN: printf("Current Direction: Down"); break;     //"Current direction: Down" output when down
+	case LEFT: printf("Current Direction: left"); break;      //"Current direction: left" output when left
+	case RIGHT: printf("Current Direction: right"); break;    //"Current direction: right" output when right
 	}
 	
 	return 0;
