5a9751f7c0aacb88f01c6ac28a9196ba140a42ee
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-22 20:27:43 +0900

    구조체들을 for문을 사용하여 입출력 할때  info++;이 표현을 해주어야 다음으로 넘어가는 것을 알게 됨

--- 9주차/P512/P512/소스.cpp
+++ 9주차/P512/P512/소스.cpp
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Student 
+typedef struct Student              //Override as Student
 {
+    //variable declaration
     int id;
     char name[20];
     int kor;
@@ -14,22 +15,22 @@ typedef struct Student
     char grade;
 };
 
-void input_data(Student* info);
-void calculation_data(Student* info);
-void print_data(Student* info);
-void sort_data(Student* info);
+void input_data(Student* info);     //Input function
+void calculation_data(Student* info);     //Total Score Mean Credit Calculation Function
+void print_data(Student* info);        //Output function
+void sort_data(Student* info);          //A function to sort
 int main(void)
 {
-    Student info[5];
+    Student info[5];                 //Declare the new datatype as an info array
     
-    input_data(info);
-    calculation_data(info);
+    input_data(info);               //Put info array in input function
+    calculation_data(info);          //Calculation info array in input function
     
     printf("#Data Before Sort...\n");
-    print_data(info);
-    sort_data(info);
-    printf("#Data After Sort...\n");
-    print_data(info);
+    print_data(info);                  //Put info array in output function
+    sort_data(info);                    //Put info array in sort function
+    printf("#Data After Sort...\n"); 
+    print_data(info);                  //Putting sorted data into the output function
 
     return 0;
 }
@@ -38,17 +39,17 @@ void input_data(Student* info)
 {
     int i;
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)         //For statements that enter variable values stored in the structure in the info array
     {
         printf("id : ");
-        scanf_s("%d", &info->id);
+        scanf_s("%d", &info->id);   
 
         printf("name: ");
         scanf("%s", info->name);
 
         printf("Kor, Eng, Mat : ");
         scanf_s("%d%d%d", &info->kor, &info->eng, &info->mat);
-        info++;
+        info++;                     //Meaning Moving On to Array
     }
 }
 void calculation_data(Student* info)
@@ -59,48 +60,49 @@ void calculation_data(Student* info)
     {
         info->tot = info->kor + info->eng + info->mat;
         info->avg = info->tot / 3.0;
-        if (info->avg >= 90.0)
+        if (info->avg >= 90.0)               //If it's 90 or higher, A
         {
-            info->grade = 'A';
+            info->grade = 'A'; 
         }
-        else if (info->avg >= 80.0)
+        else if (info->avg >= 80.0)           //If it's 80 or higher, B
         {
             info->grade = 'B';
         }
-        else if (info->avg >= 70.0)
+        else if (info->avg >= 70.0)        //If it's 80 or higher, C
         {
             info->grade = 'C';
-        }
-        else
+        } 
+        else                             //If it doesn't apply to all of them, F
         {
             info->grade = 'F';
         }
-        info++;
+        info++;          //Meaning Moving On to Array
     }
 }
 void print_data(Student* info)
 {
     int i;
-    
-    for (i = 0; i < 5; i++)
+     
+    for (i = 0; i < 5; i++)          //To print out the information stored in the info
     {
         printf("%5d%5s%5d%5d%5d%5d%5.1If%5c\n", info->id, info->name, info->kor, info->eng, info->mat, info->tot, info->avg, info->grade);
-        info++;
+        info++;                 // //Meaning Moving On to Array
     }
 }
 void sort_data(Student* info)
 {
     Student temp;
     int i, j, k;
-
+     
+    //Use descending double for statements to align
     for (i = 0; i < 4; i++)
     {
         k = i;
-        for (j = i + 1; j < 5; j++)
+        for (j = i + 1; j < 5; j++)             //Application of descending and ascending programs previously reported
         {
             if (info[k].tot < info[j].tot)
             {
-                k = j;
+                k = j; 
             }
         }
         if (k != i)
