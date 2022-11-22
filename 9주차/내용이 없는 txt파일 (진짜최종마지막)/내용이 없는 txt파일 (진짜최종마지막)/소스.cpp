e4d0eebdb3576d893af2e9a7bb5c604c46730a89
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-22 21:04:23 +0900

    분명 출력이 다 되었는데 자꾸파일이 망가짐..수업 때 교수님께 여쭤봤는데 처음 보는 오류라고 하심

---
+++ 9주차/내용이 없는 txt파일 (진짜최종마지막)/내용이 없는 txt파일 (진짜최종마지막)/소스.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+
+	FILE* file;
+
+	int integer = 1;
+	double real_number = 1.2;
+	char character = 'a';
+	char string[20] = { "Hi" };
+
+	fopen_s(&file, "b.txt", "w");
+
+	if (file == NULL)
+	{
+		printf("File did not open/\n ");
+		return 1;
+	}
+	
+	printf("%d\n%.1If\n %c\n %s", integer, real_number, character, string);
+	fprintf(file, "%d\n%.1If\n %c\n %s", integer, real_number, character, string);
+
+
+	fclose(file);
+
+	return 0;
+}
