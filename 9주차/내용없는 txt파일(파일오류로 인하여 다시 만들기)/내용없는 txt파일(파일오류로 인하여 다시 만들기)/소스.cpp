b103cb8144116b459a618ee5423a2346e59fa4f8
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-22 14:46:34 +0900

    .

--- 9주차/내용없는 txt파일(파일오류로 인하여 다시 만들기)/내용없는 txt파일(파일오류로 인하여 다시 만들기)/소스.cpp
+++ 9주차/내용없는 txt파일(파일오류로 인하여 다시 만들기)/내용없는 txt파일(파일오류로 인하여 다시 만들기)/소스.cpp
@@ -13,7 +13,7 @@ int main(void)
 	double real_number = 1.2;
 	char character = 'a';
 	char string[20] = { "Hi" };
-	
+
 	fopen_s(&file, "b.txt", "w");
 
 	if (file == NULL)
@@ -21,11 +21,10 @@ int main(void)
 		printf("File did not open/\n ");
 		return 1;
 	}
+	printf("%d\n%.1If\n %c\n %s", integer, real_number, character, string);
+	fprintf(file, "%d\n%.1If\n %c\n %s", integer, real_number, character, string);
 
-	fprintf(file, "%d %If %c %s", integer, real_number, character, string);
 
-	
-	
 	fclose(file);
 
 	return 0;
