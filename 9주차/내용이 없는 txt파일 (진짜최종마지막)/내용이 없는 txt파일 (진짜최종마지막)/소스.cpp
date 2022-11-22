8ca602de8c5dbaf38fb230e1ca273593ac517e3c
Sonyoungjin <ysjsjs1000@naver.com>
2022-11-22 21:11:21 +0900

    파일을 다 완성하고 자꾸 알수 없는 오류들이 나와 파일을 다시 만듬. 수업 중에 교수님께 여쭤봤는데 처음 보는 오류라고 하심

--- 9주차/내용이 없는 txt파일 (진짜최종마지막)/내용이 없는 txt파일 (진짜최종마지막)/소스.cpp
+++ 9주차/내용이 없는 txt파일 (진짜최종마지막)/내용이 없는 txt파일 (진짜최종마지막)/소스.cpp
@@ -5,26 +5,27 @@
 int main(void)
 {
 
-	FILE* file;
+	FILE* file;       //File Pointer Declaration
 
-	int integer = 1;
+	//Declare variation
+	int integer = 1;     
 	double real_number = 1.2;
 	char character = 'a';
 	char string[20] = { "Hi" };
 
-	fopen_s(&file, "b.txt", "w");
+	fopen_s(&file, "b.txt", "w");       //The code that generates the file
 
-	if (file == NULL)
+	if (file == NULL)                 //Check if it is open
 	{
 		printf("File did not open/\n ");
 		return 1;
 	}
 	
-	printf("%d\n%.1If\n %c\n %s", integer, real_number, character, string);
-	fprintf(file, "%d\n%.1If\n %c\n %s", integer, real_number, character, string);
+	printf("%d\n%.1If\n %c\n %s", integer, real_number, character, string);        //Output to Console
+	fprintf(file, "%d\n%.1If\n %c\n %s", integer, real_number, character, string);   //Output to the file you created
 
 
-	fclose(file);
+	fclose(file);       //Close File
 
 	return 0;
 }
